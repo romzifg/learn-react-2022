57842efc487996353c385ddcd8b0c0f536e21357
Romzi Farhan Ghozi <romzifg@gmail.com>
2022-08-29 22:07:29 +0700

    update

--- 2. crown-clothing/src/utils/firebase/firebase.js
+++ 2. crown-clothing/src/utils/firebase/firebase.js
@@ -1,6 +1,11 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
-
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
 
 // firebase configuration
 const firebaseConfig = {
@@ -22,3 +27,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+
+    console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef)
+    console.log(userSnapshot)
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error createing user', error.message)
+        }
+    }
+
+    return userDocRef;
+}
