459b237218337f61603e5de589c676c6b42450c9
Romzi Farhan Ghozi <romzifg@gmail.com>
2022-09-01 20:24:17 +0700

    update layout authentication

--- 2. crown-clothing/src/App.js
+++ 2. crown-clothing/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
+import Authentication from './layout/authentication/authentication';
 import Home from "./layout/home/home";
 import Navigation from './layout/navigation/navigation';
-import SignIn from './layout/signin/sign-in';
 
 const Shop = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   )
