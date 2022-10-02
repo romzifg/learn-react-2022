61fa40d634a876b24dd8994403302ca0fc25ac0a
Romzi Farhan Ghozi <romzifg@gmail.com>
2022-10-02 13:22:07 +0700

    update preview

--- 02. crown-clothing/src/layout/shop/shop.jsx
+++ 02. crown-clothing/src/layout/shop/shop.jsx
@@ -1,23 +1,19 @@
 import { useContext } from "react";
 import { CategoryContext } from "../../context/categories.context";
-import ProductCard from "../../components/product-cart/product-card";
+import CategoryPreview from "../../components/category-preview/category-preview";
 import "./shop.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoryContext);
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <div key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      ))}
-    </>
+    <div className="shop-caontainer">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
