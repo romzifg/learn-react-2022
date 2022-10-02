b85e06508699f33f227a2e19a44030a883aa67c4
Romzi Farhan Ghozi <romzifg@gmail.com>
2022-10-02 13:49:46 +0700

    update component & routes

--- 02. crown-clothing/src/components/category-preview/category-preview.jsx
+++ 02. crown-clothing/src/components/category-preview/category-preview.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ProductCard from "../product-cart/product-card";
+import { Link } from "react-router-dom";
 import "./category-preview.scss";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link className="title" to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className="preview">
         {products
