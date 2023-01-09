c85612d84f9141d53bae9e43c8e51d3a046f7612
Romzi Farhan Ghozi <romzifg@gmail.com>
2023-01-09 22:29:32 +0700

    update

--- 03. crown-clothing-redux/src/store/categories/category.selector.js
+++ 03. crown-clothing-redux/src/store/categories/category.selector.js
@@ -1,26 +1,17 @@
 import { createSelector } from 'reselect';
 
-const selectCategoryReducer = (state) => {
-    console.log('selector 1 fired')
-    return state.categories
-};
+const selectCategoryReducer = (state) => state.categories;
 
 export const selectCategories = createSelector(
     [selectCategoryReducer],
-    (categoriesSlice) => {
-        console.log('selector 2 fired')
-        return categoriesSlice.categories
-    }
+    (categoriesSlice) => categoriesSlice.categories
 )
 
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => {
-        console.log('selector 3 fired')
-        return categories.reduce((acc, category) => {
-            const { title, items } = category;
-            acc[title.toLowerCase()] = items;
-            return acc;
-        }, {})
-    }
+    (categories) => categories.reduce((acc, category) => {
+        const { title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {})
 )
